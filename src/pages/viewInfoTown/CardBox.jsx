4017227a529a9d439438a96791275d2a5bf31922
J-Daniel-Bedoya <jbedoyachavarriaga@gmail.com>
2024-05-19 22:37:52 -0500

    progresos en la app

--- src/pages/viewInfoTown/CardBox.jsx
+++ src/pages/viewInfoTown/CardBox.jsx
@@ -1,29 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-// import getConfig from "../../utils/getConfig";
-
-// const CardBox = ({ box }) => {
-//   const [sector, setSector] = useState();
-//   const api = "http://localhost:8000/api/v1";
-
-//   useEffect(() => {
-//     axios.get(`${api}/sector/${box.sectorId}`, getConfig()).then((res) => {
-//       setSector(res.data);
-//     });
-//   }, []);
-
-//   return (
-//     <div className="cards__list">
-//       <p className="box">{box.numberBox}</p>
-//       <p className="port">{box.numberPorts}</p>
-//       <p className="portsUsed">{box.numberUsers}</p>
-//       <p className="sector">{sector?.sectorName}</p>
-//     </div>
-//   );
-// };
-
-// export default CardBox;
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import getConfig from "../../utils/getConfig";
