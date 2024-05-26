296d5fc02b7760a1b1ae6bb7af27c9e7943c89df
J-Daniel-Bedoya <jbedoyachavarriaga@gmail.com>
2024-05-26 18:21:20 -0500

    aa

--- src/components/total/Total.jsx
+++ src/components/total/Total.jsx
@@ -2,6 +2,13 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getServicesThunk } from "../../store/slices/service.slice";
 
+const iconMapping = {
+  Internet: "fas fa-wifi",
+  TV: "fas fa-tv",
+  Combo: "fas fa-box",
+  default: "fas fa-concierge-bell",
+};
+
 const Total = () => {
   const town = useSelector((state) => state.town);
   const services = useSelector((state) => state.service);
@@ -9,18 +16,48 @@ const Total = () => {
 
   useEffect(() => {
     dispatch(getServicesThunk());
-  }, []);
+  }, [dispatch]);
 
   return (
-    <div>
-      <p>
-        Numbero de Clientes: <b>{town.numberUsers}</b>
-      </p>
-      {services.map((service) => (
-        <p>
-          Total de {service.serviceName}: <b>{service?.numberUsers}</b>
-        </p>
-      ))}
+    <div className="total">
+      <div className="total__header">
+        <h2>Información Total</h2>
+      </div>
+      <table className="total__table">
+        <thead>
+          <tr>
+            <th>
+              <i className="fas fa-info-circle"></i> Tipo
+            </th>
+            <th>
+              <i className="fas fa-users"></i> Total
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Número de Clientes</td>
+            <td>
+              <b>{town.numberUsers}</b>
+            </td>
+          </tr>
+          {services.map((service) => (
+            <tr key={service.id}>
+              <td>
+                <i
+                  className={
+                    iconMapping[service.serviceName] || iconMapping.default
+                  }
+                ></i>{" "}
+                Total de {service.serviceName}
+              </td>
+              <td>
+                <b>{service?.numberUsers}</b>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
