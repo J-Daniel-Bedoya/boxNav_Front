ed70bd802e577fef3aa51de3e4cf21d554c25ae9
J-Daniel-Bedoya <jbedoyachavarriaga@gmail.com>
2024-06-23 16:42:38 -0500

    primera version de la app terminada

--- src/components/search/SearchUser.jsx
+++ src/components/search/SearchUser.jsx
@@ -1,38 +1,36 @@
-// SearchUser.js
 import React from "react";
 import { useForm } from "react-hook-form";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import SearchResults from "./SearchResults";
 import useSearchUser from "../../hooks/search/useSearchUser";
 import { setSearchResults } from "../../store/slices/user.slice";
 
 const SearchUser = () => {
-  const { register, watch } = useForm();
+  const { register, handleSubmit, watch } = useForm();
   const dispatch = useDispatch();
-  const { query, setQuery, results, loading } = useSearchUser();
+  const { query, setQuery, results, loading, fetchResults } = useSearchUser();
+
+  const onSubmit = (data) => {
+    // Limpiar resultados antes de realizar una nueva búsqueda
+    dispatch(setSearchResults([]));
+    fetchResults(data.username);
+  };
 
   const inputValue = watch("username");
 
-  const handleChange = (e) => {
-    setQuery(e.target.value);
-    if (e.target.value.length === 0) {
-      dispatch(setSearchResults([]));
-    }
-  };
-
   return (
     <div className="search-user">
-      <form className="search-user__form">
+      <form className="search-user__form" onSubmit={handleSubmit(onSubmit)}>
         <input
           type="text"
           {...register("username")}
           placeholder="Buscar por Usuario"
           className="search-user__input"
           value={query}
-          onChange={handleChange}
+          onChange={(e) => setQuery(e.target.value)}
         />
         <button
-          type="button"
+          type="submit"
           className="search-user__button"
           disabled={loading}
         >
--- src/hooks/search/useSearchUser.jsx
+++ src/hooks/search/useSearchUser.jsx
@@ -1,39 +1,33 @@
-// useSearchUser.js
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import getConfig from "../../utils/getConfig";
+import { useDispatch } from "react-redux";
+import { setSearchResults } from "../../store/slices/user.slice";
 
 const api = "https://nav-boxes-lis.up.railway.app/api/v1";
 
 const useSearchUser = () => {
+  const dispatch = useDispatch();
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const delayDebounceFn = setTimeout(() => {
-      if (query.length > 0) {
-        setLoading(true);
-        axios
-          .get(`${api}/user/search?query=${query}`, getConfig())
-          .then((res) => {
-            setResults(res.data);
-            setLoading(false);
-          })
-          .catch((error) => {
-            console.error("Error fetching search results:", error);
-            setResults([]);
-            setLoading(false);
-          });
-      } else {
-        setResults([]);
-      }
-    }, 300);
+  const fetchResults = (username) => {
+    setLoading(true);
+    axios
+      .get(`${api}/user/search?query=${username}`, getConfig())
+      .then((res) => {
+        setResults(res.data);
+        dispatch(setSearchResults(res.data));
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching search results:", error);
+        setLoading(false);
+      });
+  };
 
-    return () => clearTimeout(delayDebounceFn);
-  }, [query]);
-
-  return { query, setQuery, results, loading };
+  return { query, setQuery, results, loading, fetchResults };
 };
 
 export default useSearchUser;
